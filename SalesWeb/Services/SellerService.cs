726a25877326bbda60662bb58cf92bd5d834647d
Pietro Ferreira <piiiietro.pereira@hotmail.com>
2019-10-11 21:57:14 -0300

    Preparing sales search navigation views

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -41,7 +41,7 @@ namespace SalesWeb.Services
                 var obj = await _context.Seller.FindAsync(id);
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
-            }
+            } 
             catch(DbUpdateException e)
             {
                 throw new IntegrityException("Can't delete seller because he/she has sales");
